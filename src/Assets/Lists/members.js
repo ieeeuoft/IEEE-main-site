f6983d3ee6f7595bafe8613b4f42900053d35e87
Alex <stefanalexb@gmail.com>
2019-08-01 00:30:40 -0400

    Tests with a JSON implementation (doesn't work 100%)

---
+++ src/Assets/Lists/members.js
@@ -0,0 +1,40 @@
+const members = {
+     "current": [
+          {"execs": [
+               { "fullName": "Nhien Tran-Nyguen", "position": "Branch Chair", "year": "2019_20"},
+               { "fullName": "Fumi Shibutani", "position": "VC Finance", "year": "2019_20"},
+               { "fullName": "Maggie Ding", "position": "VC Marketing", "year": "2019_20"},
+               { "fullName": "Sirine Dhaouadi", "position": "VC External", "year": "2019_20"},
+               { "fullName": "Danny Zhang", "position": "Branch Advisor", "year": "2019_20"},
+               { "fullName": "Kevin Zhang", "position": "Branch Advisor", "year": "2019_20"}
+          ]},
+          {"electronics": [
+               { "fullName": "Lisa Li", "position": "Co-Chair", "year": "2019_20"},
+               { "fullName": "Martin Ffrench", "position": "Co-Chair", "year": "2019_20"},
+               { "fullName": "Tyson Dennis-Sharma", "position": "Finance Director", "year": "2019_20"},
+               { "fullName": "Jennifer Kong", "position": "Director", "year": "2019_20"},
+               { "fullName": "Nhien Tran-Nyguen", "position": "Branch Chair", "year": "2019_20"},
+               { "fullName": "Fumi Shibutani", "position": "VC Finance", "year": "2019_20"},
+               { "fullName": "Maggie Ding", "position": "VC Marketing", "year": "2019_20"},
+               { "fullName": "Sirine Dhaouadi", "position": "VC External", "year": "2019_20"},
+               { "fullName": "Danny Zhang", "position": "Branch Advisor", "year": "2019_20"},
+               { "fullName": "Kevin Zhang", "position": "Branch Advisor", "year": "2019_20"}
+          ]},
+          {"computer": [
+               { "fullName": "Lisa Li", "position": "Co-Chair", "year": "2019_20"},
+               { "fullName": "Martin Ffrench", "position": "Co-Chair", "year": "2019_20"},
+               { "fullName": "Tyson Dennis-Sharma", "position": "Finance Director", "year": "2019_20"},
+               { "fullName": "Jennifer Kong", "position": "Director", "year": "2019_20"},
+               { "fullName": "Nhien Tran-Nyguen", "position": "Branch Chair", "year": "2019_20"},
+               { "fullName": "Fumi Shibutani", "position": "VC Finance", "year": "2019_20"},
+               { "fullName": "Maggie Ding", "position": "VC Marketing", "year": "2019_20"},
+               { "fullName": "Sirine Dhaouadi", "position": "VC External", "year": "2019_20"},
+               { "fullName": "Danny Zhang", "position": "Branch Advisor", "year": "2019_20"},
+               { "fullName": "Kevin Zhang", "position": "Branch Advisor", "year": "2019_20"}
+          ]}
+
+     ]
+
+};
+
+export default members;
