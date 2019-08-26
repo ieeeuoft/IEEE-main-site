f21a616380337f11054fcea54fea5d7af187fad8
Lisa Li <>
2019-08-26 18:26:19 -0400

    Added Munvo as sponsor

--- src/Assets/Lists/allSponsors.js
+++ src/Assets/Lists/allSponsors.js
@@ -2,5 +2,6 @@ export const allSponsors = [
     { companyName: "Qualcomm", tier: "Platinum - MakeUofT", imgType: ".svg", siteLink: "https://www.qualcomm.com/"},
     { companyName: "Google", tier: "Event Partner", imgType: ".svg", siteLink: "https://about.google"},
     { companyName: "PTI Transformers", tier: "Bronze - MakeUofT", imgType: ".png", siteLink: "https://www.partnertechnologies.net/" },
-    { companyName: "Daisy Intelligence", tier: "Gold - Hello, Con!", imgType: ".svg", siteLink: "https://www.daisyintelligence.com/" }
+    { companyName: "Daisy Intelligence", tier: "Gold - Hello, Con!", imgType: ".svg", siteLink: "https://www.daisyintelligence.com/" },
+    { companyName: "Munvo", tier: "Gold - Hello, Con!", imgType: ".png", siteLink: "https://munvo.com/" }
 ]
