a65fb35d4b129ff3312228c397703064950f67d7
Lisa Li <>
2019-08-26 23:55:57 -0400

    added links of new sponsors

--- src/Assets/Lists/allSponsors.js
+++ src/Assets/Lists/allSponsors.js
@@ -1,10 +1,10 @@
 export const allSponsors = [
     { companyName: "Qualcomm", tier: "Platinum - MakeUofT", imgType: ".svg", siteLink: "https://www.qualcomm.com/"},
     { companyName: "PTI Transformers", tier: "Bronze - MakeUofT", imgType: ".png", siteLink: "https://www.partnertechnologies.net/" },
-    { companyName: "HP", tier: "Prize Sponsor - MakeUofT", imgType: ".svg", siteLink: "https://munvo.com/" },
-    { companyName: "BITalino", tier: "Prize Sponsor - MakeUofT", imgType: ".png", siteLink: "https://munvo.com/" },
+    { companyName: "HP", tier: "Prize Sponsor - MakeUofT", imgType: ".svg", siteLink: "https://www.hpe.com/ca/en/home.html" },
+    { companyName: "BITalino", tier: "Prize Sponsor - MakeUofT", imgType: ".png", siteLink: "https://bitalino.com/en/" },
     { companyName: "Google", tier: "Event Partner", imgType: ".svg", siteLink: "https://about.google"},
-    { companyName: "AMD", tier: "Event Partner", imgType: ".svg", siteLink: "https://munvo.com/" },
+    { companyName: "AMD", tier: "Event Partner", imgType: ".svg", siteLink: "https://www.amd.com/en" },
     { companyName: "Daisy Intelligence", tier: "Gold - Hello, Con!", imgType: ".svg", siteLink: "https://www.daisyintelligence.com/" },
     { companyName: "Munvo", tier: "Gold - Hello, Con!", imgType: ".png", siteLink: "https://munvo.com/" }
 ]
