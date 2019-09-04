437a772c06dd6e11c309dd67663478dd49b1b619
Steven Zhao <stevenwjzhao@hotmail.com>
2019-09-04 11:16:04 -0400

    Changed google form link to http://eepurl.com/7spfT

--- src/Components/Home/Social/Social.js
+++ src/Components/Home/Social/Social.js
@@ -28,9 +28,9 @@ const Social = () => {
             <ScrollAnimation animateOnce={true} duration={1} offset={80} animateIn="fadeInUp" className={styles['membership-text']}>
                 <a href="https://www.ieee.org/content/ieee-org/en/membership/join/">More than 80% discount for IEEE memberships and receive various exclusive benefits including job opportunities!</a>
             </ScrollAnimation>
-            
+            {/*https://goo.gl/forms/dyVbc0fyJAZJ0Bbv1*/}
             <a href="https://www.facebook.com/ieeeuoft/" className={styles['facebook']} target="_blank"></a>
-            <a href="https://goo.gl/forms/dyVbc0fyJAZJ0Bbv1" className={styles['newsletter']} target="_blank"></a>
+            <a href="http://eepurl.com/7spfT" className={styles['newsletter']} target="_blank"></a>
             <a href="https://www.ieee.org/membership/join" className={styles['membership']} target="_blank"></a>
         </div>
     )
