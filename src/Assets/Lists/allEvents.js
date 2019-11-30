b5bf8754afa216bb30063bef1500dc1d31a68242
lisa-sa-li <lisasiaili@gmail.com>
2019-11-29 22:33:40 -0500

    changed event date

--- src/Assets/Lists/allEvents.js
+++ src/Assets/Lists/allEvents.js
@@ -33,21 +33,21 @@ export const allEvents = [
         },
     { 
         EventName: "Hardware Introduction Workshop",
-        date: "2020/01/18",
-        displayDate: "January 18th, 2020", 
+        date: "2020/01/11",
+        displayDate: "January 11th, 2020", 
         // link: "www.facebook.com/ieeeuoft",
         },
-    { 
-        EventName: "Hardware Certification Workshop",
-        displayDate: "January 25th, 2020", 
-        date: "2020/01/25",
-        // link: "www.facebook.com/ieeeuoft" ,
-        },
+    // { 
+    //     EventName: "Hardware Certification Workshop",
+    //     displayDate: "January 25th, 2020", 
+    //     date: "2020/01/25",
+    //     // link: "www.facebook.com/ieeeuoft" ,
+    //     },
     { 
         EventName: "MakeUofT",
         displayDate: "February 15-16, 2020",
         date: "2020/02/15", 
-        link: "www.instagram.com/makeuoft" 
+        link: "ieee.utoronto.ca/makeuoft/" 
         },
     { 
         EventName: "NewHacks",
