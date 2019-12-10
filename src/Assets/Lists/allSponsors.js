862841256a13049148117b9386cfbd767001be63
lisa-sa-li <lisasiaili@gmail.com>
2019-12-10 16:47:59 -0500

    Added new sponsor

--- src/Assets/Lists/allSponsors.js
+++ src/Assets/Lists/allSponsors.js
@@ -1,10 +1,11 @@
 export const allSponsors = [
-    { companyName: "IEEE Toronto", tier: "Branch-Level Partner", imgType: ".png", siteLink: "https://toronto.ieee.ca/"},
+    { companyName: "IEEE Toronto", tier: "Branch-Level Partner", imgType: ".svg", siteLink: "https://toronto.ieee.ca/"},
     { companyName: "IEEE Canada", tier: "Branch-Level Partner", imgType: ".png", siteLink: "https://www.ieee.ca/en/"},
     { companyName: "Qualcomm", tier: "Platinum - MakeUofT", imgType: ".svg", siteLink: "https://www.qualcomm.com/"},
     { companyName: "ECE", tier: "Gold - MakeUofT", imgType: ".png", siteLink: "https://www.partnertechnologies.net/" },
+    { companyName: "Faculty of Applied Science and Engineering", tier: "Gold - MakeUofT", imgType: ".png", siteLink: "https://www.engineering.utoronto.ca/" },
+    { companyName: "Xilinx", tier: "Silver - MakeUofT", imgType: ".svg", siteLink: "https://www.xilinx.com/" },
     { companyName: "PTI Transformers", tier: "Bronze - MakeUofT", imgType: ".png", siteLink: "https://www.partnertechnologies.net/" },
-    { companyName: "Faculty of Applied Science and Engineering", tier: "Bronze - MakeUofT", imgType: ".png", siteLink: "https://www.partnertechnologies.net/" },
     { companyName: "HP", tier: "Prize Sponsor - MakeUofT", imgType: ".svg", siteLink: "https://www.hpe.com/ca/en/home.html" },
     { companyName: "BITalino", tier: "Prize Sponsor - MakeUofT", imgType: ".png", siteLink: "https://bitalino.com/en/" },
     { companyName: "Google", tier: "Event Partner", imgType: ".svg", siteLink: "https://about.google"},
